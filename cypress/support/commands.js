c8111e2717f7beb0bf14c8eab2290f8282adc2c6
Kinkini <kinkinigamage25@gmail.com>
2025-04-19 16:00:03 +1000

    ci setup

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,9 @@
-Cypress.Commands.add('UserLogin', (user) => { 
+Cypress.Commands.add('UserLogin', (apple) => { 
     cy.contains('a', 'Signup / Login').click()
     cy.url().should('include', '/login');
     cy.contains('h2', 'Login to your account').should('be.visible')
-    cy.get('[data-qa="login-email"]').type('kinkini3@mail.com')
-    cy.get('[data-qa="login-password"]').type('12345')
+    cy.get('[data-qa="login-email"]').type(apple.email)
+    cy.get('[data-qa="login-password"]').type(apple.password)
     cy.get('[data-qa="login-button"]').click()
     cy.url().should('include', '/automationexercise');
   })
